56a105955cf0cabac3cecdcc5369b864ef1a5d94
kgoel085 <kgoel085@gmail.com>
2018-10-28 17:22:01 +0530

    Initialized Vuex Object

---
+++ src/store/store.js
@@ -0,0 +1,19 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+    state:{
+        CLIENT_API_KEY: 'AIzaSyAzUuj9WJ7pPRYG3tfUkIJioL-Tgt34JNM'
+    },
+    getters:{
+        
+    },
+    mutations:{
+
+    },
+    actions:{
+
+    }
+});
