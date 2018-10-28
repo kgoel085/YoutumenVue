6f1a042e69f34ec0dc7c176ff0fa048c3c407b4f
kgoel085 <kgoel085@gmail.com>
2018-10-29 00:48:55 +0530

    Added Sidebar component

--- src/App.vue
+++ src/App.vue
@@ -3,22 +3,50 @@
     <!-- Youtube style search bar -->
     <app-header></app-header>
 
-    <div class="container">
+    <!-- Sidebar -->
+    <template v-if="sideBar">
+        <app-sidebar></app-sidebar>
+    </template>
+
+    <div class="container-fluid" :class="{overlay: sideBar}">
+      <!-- Main Content View -->
       <router-view/>
     </div>
+
+
   </div>
 </template>
 
 <script>
 import Header from './components/Header.vue';
+import Sidebar from './components/Sidebar.vue';
 
 export default {
   name: 'App',
+  data(){
+    return{
+      
+    }
+  },
+  computed:{
+    sideBar(){
+      //GEt Global state of sidear and Show it based on it 
+      return this.$store.getters.GET_SIDEBAR;
+    }
+  },
   components:{
-    'app-header': Header
+    'app-header': Header,
+    'app-sidebar': Sidebar
   }
 }
 </script>
 
-<style>
+<style >
+  .overlay {
+      background: #ccc;
+      height: 100%!important;
+      width: 100%!important;
+      position: fixed;
+      transition: all 0.5s ease;
+  }
 </style>
