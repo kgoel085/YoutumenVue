63af789658a4f74cad38e5fd48946356300cf459
kgoel085 <kgoel085@gmail.com>
2018-10-28 17:20:12 +0530

    Added Vuex Object

--- src/main.js
+++ src/main.js
@@ -4,11 +4,15 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+//Vuex Object instance
+import store from './store/store'
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  store,
   router,
   components: { App },
   template: '<App/>'
