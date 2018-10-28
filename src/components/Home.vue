e55ebd845ea6be3606c8a174fa0de6fa29e1f6e1
kgoel085 <kgoel085@gmail.com>
2018-10-28 22:16:55 +0530

    Added Home component

---
+++ src/components/Home.vue
@@ -0,0 +1,28 @@
+<template>
+    
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+           
+        }
+    },
+    computed:{
+        videos(){
+            //this.$http.get('videoCategories',{params: {'regionCode': 'US'}}).then(response => response.json()).then(resp => {
+            // this.$http.get('videos',{params: {'maxResults': '5', 'chart': 'mostPopular'}}).then(response => response.json()).then(resp => {
+            //     console.log(resp);
+            // }); 
+        }
+    },
+    methods:{
+        
+    }
+}
+</script>
+
+<style>
+
+</style>
