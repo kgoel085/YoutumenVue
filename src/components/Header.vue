92ad9d54a94dbfad45e371fb6fb4a5a35439e98d
kgoel085 <kgoel085@gmail.com>
2018-10-29 00:50:18 +0530

    Added sidebar toggle button and added methods to manage sidebar global state

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,31 +1,40 @@
 <template>
     <nav class="navbar navbar-default">
         <div class="container-fluid">
-            <!-- Brand and toggle get grouped for better mobile display -->
+             <!-- Brand and toggle get grouped for better mobile display -->
             <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" @click="toggledMenu = !toggledMenu">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <button class="sideToggle" type="button" @click="sideBarState = !sideBarState"><i class="glyphicon glyphicon-menu-hamburger"></i></button>
                 <router-link tag="a" class="navbar-brand" :to="{name: 'Home'}">
+                    
                     <img src="../assets/logo.png" style="width:100px;height:auto">
                 </router-link>
             </div>
+            
 
             <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <ul class="nav navbar-nav">
+            <div class="collapse navbar-collapse" :class="{in: toggledMenu}" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
 
-            </ul>
-            <form class="navbar-form navbar-left">
-                <div class="input-group">
-                    <input type="text" class="form-control" placeholder="Search for...">
-                    <span class="input-group-btn">
-                        <button class="btn btn-default" type="button">
-                            <i class="glyphicon glyphicon-search"></i>
-                        </button>
-                    </span>
-                </div>
-            </form>
-            <ul class="nav navbar-nav navbar-right">
+                </ul>
+                <form class="navbar-form navbar-left">
+                    <div class="input-group">
+                        <input type="text" class="form-control" placeholder="Search for...">
+                        <span class="input-group-btn">
+                            <button class="btn btn-default" type="button">
+                                <i class="glyphicon glyphicon-search"></i>
+                            </button>
+                        </span>
+                    </div>
+                </form>
+                <ul class="nav navbar-nav navbar-right">
 
-            </ul>
+                </ul>
             </div><!-- /.navbar-collapse -->
         </div><!-- /.container-fluid -->
     </nav>
@@ -33,10 +42,32 @@
 
 <script>
 export default {
-
+    data(){
+        return{
+            toggledMenu: false,
+            sideBarState: this.$store.getters.GET_SIDEBAR
+        }
+    },
+    watch:{
+        //Change Glboal sidebar state to check whetehr to open it or not
+        sideBarState(value){
+            this.$store.dispatch('SET_SIDEBAR_VIEW', value);
+        }
+    }
 }
 </script>
 
 <style scoped>
-
+    .navbar{margin-bottom: 0px}
+    .menuBtn{color:#80808066}
+    .menuBtn>.icon{font-size:20px}
+    .sideToggle{
+        padding: 7px 8px;
+        float: left;
+        /* border-radius: 0px 5px 5px 0px; */
+        margin: 10px;
+        background: transparent;
+        border: none;
+        border-radius: 5px;
+    }
 </style>
