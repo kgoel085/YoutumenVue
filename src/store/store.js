2d1bcc9d7f4ed932408cec6baa00597cfb6efb4e
kgoel085 <kgoel085@gmail.com>
2018-10-29 00:52:49 +0530

    Added global methods to manage sidebar state globally

--- src/store/store.js
+++ src/store/store.js
@@ -5,15 +5,21 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
     state:{
-        CLIENT_API_KEY: ''
+        sidebarToggle: false
     },
     getters:{
-        
+        'GET_SIDEBAR':state => {
+            return state.sidebarToggle;
+        }
     },
     mutations:{
-
+        'SET_SIDEBAR':(state, param) => {
+            state.sidebarToggle = param;
+        }
     },
     actions:{
-
+        'SET_SIDEBAR_VIEW':(context, param) => {
+            context.commit('SET_SIDEBAR', param);
+        }
     }
 });
