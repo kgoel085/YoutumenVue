9356bcdba5ae8b751dac8a29cb5de70d990f6d0c
kgoel085 <kgoel085@gmail.com>
2018-10-30 01:22:28 +0530

    Added static sidebar content and CSS

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -3,7 +3,26 @@
         <div id="mainSideBar" :style="{height: parentHeight+'px'}">
             <div class="row">
                 <div class="col-md-12">
+                    <ul class="sideMenu">
+                        <li>
+                            <i class="glyphicon glyphicon-home icon"></i>
+                            <a>Home</a>
+                        </li>
+                        <li>
+                            <i class="glyphicon glyphicon-fire icon"></i>
+                            <a>Trending</a>
+                        </li>
+                        <li>
+                            <i class="glyphicon glyphicon-folder-close icon"></i>
+                            <a>Subscription</a>
+                        </li>
+                    </ul>
 
+                    <ul class="sideMenu">
+                        <li class="heading">
+                            <a >Library</a>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </div>
@@ -52,5 +71,43 @@ export default {
     position: absolute;
     left: 0;
     z-index: 99;
+}
+
+.sideMenu{
+    list-style-type:none;
+    margin: 10px 0px 10px 0px;
+    padding: 0px;
+    font-size:16px;
+}
+
+.sideMenu li {
+    padding: 10px 34px;
+    cursor:pointer;
+}
+
+.sideMenu .heading{
+    font-weight:600;
+}
+
+.sideMenu .heading:hover{
+    background:transparent
+}
+
+.sideMenu .icon, .sideMenu i{
+    color:#888888
+}
+
+.sideMenu a{
+    padding-left: 10%;
+    font-weight: 400;
+    margin: 0;
+    vertical-align: middle;
+    text-decoration: none;
+    padding: 10px;
+    color: #4e4949!important;
+}
+
+.sideMenu li:hover{
+    background:#eae8e8d1
 }
 </style>
