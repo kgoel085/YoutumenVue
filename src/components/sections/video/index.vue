cb7e7aadf180d123ed4b36c7e66c22effac6f350
kgoel085 <kgoel085@gmail.com>
2018-11-05 01:32:16 +0530

    Added Common video components

---
+++ src/components/sections/video/index.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="col-md-12">
+        <component :is="selectedComponent" :videoObject="videoObj" :layout="layout"></component>
+    </div>
+</template>
+
+<script>
+import videoPreview from './preview.vue';
+
+export default {
+    data(){
+        return{
+            layout: 'default'
+        }
+    },
+    computed:{
+        selectedComponent(){
+            return (this.videoType !== 'preview') ? '': 'app-preview';
+        }
+    },
+    props:{
+        videoObj:{
+            type: Object,
+            default:{}
+        },
+        videoType:{
+            type: String,
+            default: 'preview'
+        }
+    },
+    components:{
+        'app-preview': videoPreview
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/sections/video/preview.vue
@@ -0,0 +1,104 @@
+<template>
+    <!-- Default layout -->
+    <div v-if="layout == 'default'">
+        <div class="col-md-12 videoBox">
+            <div class="media">
+                <div class="media-left">
+                    <a href="#">
+                    <img class="media-object" :src="currentObj.thumbnails.medium.url">
+                    </a>
+                </div>
+                <div class="media-body">
+                    <h4 class="media-heading">{{ currentObj.title }}</h4>
+                    <span class="media-content">
+                        <p><small>{{ currentObj.channelTitle }}</small> | <small>{{ currentObj.publishedAt | dateTime}}</small></p>
+                        {{ currentObj.description.substr(0, 400) + '....' }}
+                    </span>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            currentObj: this.videoObject
+        }
+    },
+    props:{
+        videoObject:{
+            default:{},
+            type: Object
+        },
+        layout:{
+            type:String
+        }
+    },
+    filters:{
+        dateTime:(value) => {
+            var months = [
+                "January", "February", "March",
+                "April", "May", "June", "July",
+                "August", "September", "October",
+                "November", "December"
+            ];
+
+            var dateVal = value.split('T');
+            if(dateVal.length) dateVal = dateVal.join(' ');
+
+            const dtObj = new Date(dateVal);
+
+            // datepart: 'y', 'm', 'w', 'd', 'h', 'n', 's'
+            Date.dateDiff = function(datepart, fromdate, todate) {	
+            datepart = datepart.toLowerCase();	
+            var diff = todate - fromdate;	
+            var divideBy = { w:604800000, 
+                            d:86400000, 
+                            h:3600000, 
+                            n:60000, 
+                            s:1000 };	
+            
+            return Math.floor( diff/divideBy[datepart]);
+            }
+
+            var today= new Date();
+            
+            //Check for Hours in less than 24 
+            var hrsPast = Date.dateDiff('h', dtObj, today);
+            if(hrsPast < 24) return hrsPast + ' hours ago';
+
+            var daysPast =  Date.dateDiff('d', dtObj, today);
+            if(daysPast < 3){
+                var endStr = ' days ago';
+                if(daysPast < 2) endStr = ' day ago';
+
+                return daysPast + endStr;
+            }
+
+            var day = dtObj.getDate();
+            var monthIndex = dtObj.getMonth();
+            var year = dtObj.getFullYear();
+
+            return day + ' ' + months[monthIndex] + ' ' + year;
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .videoBox{
+        padding:5px
+    }
+
+    .videoBox .media-content{
+        color:hsl(0, 0%, 53.3%)
+    }
+
+    .videoBox:hover{
+        background-color:#e4e2e2db;
+        transition: all 0.2s linear;
+        cursor:pointer
+    }
+</style>
