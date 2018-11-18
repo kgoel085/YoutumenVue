8aec3b50b9e12471eb28c289e43c1c4063662d4b
kgoel085 <kgoel085@gmail.com>
2018-11-18 23:08:54 +0530

    Added video playlist component

---
+++ src/components/sections/video/playlist.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="row">
+        <pre>{{ currentObj }}</pre>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            currentObj: {}
+        }
+    },
+    methods:{
+        callAPi(){
+            var vm = this;
+            
+            var paramsArr = {'part': 'snippet,contentDetails', 'playlistId': vm.videoObject.playlistId, 'maxResults': 10};
+            this.$http.get('playlistItems', {params: paramsArr}).then(resp => resp.json()).then(response => {
+                console.log(response);
+            });
+        }
+    },
+    created(){
+        this.callAPi();
+    },
+    props:{
+        videoObject:{
+            default:{},
+            type: Object
+        },
+        layout:{
+            type:String
+        }
+    },
+}
+</script>
+
+<style>
+
+</style>
