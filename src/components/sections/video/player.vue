9e645ac246677a0f96d5605c178da51bbf79167d
kgoel085 <kgoel085@gmail.com>
2018-12-20 01:37:56 +0530

    Added a new video player component

---
+++ src/components/sections/video/player.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        Received Video Id: {{videoId}}  
+    </div>    
+</template>
+
+<script>
+export default {
+    computed:{
+        videoId(){
+            return this.$route.params.id;
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
