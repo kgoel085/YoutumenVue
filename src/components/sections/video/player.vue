f03b7c778be461e771c01085f2e1c70fc170572c
kgoel085 <kgoel085@gmail.com>
2018-12-22 01:22:34 +0530

    Changed inner logic and layout

--- src/components/sections/video/player.vue
+++ src/components/sections/video/player.vue
@@ -1,15 +1,85 @@
 <template>
-    <div>
-        Received Video Id: {{videoId}}  
-    </div>    
+    <div class="row">
+
+    </div>
 </template>
 
 <script>
+import { configArr } from '../../../classes/configuration';
+
 export default {
-    computed:{
-        videoId(){
-            return this.$route.params.id;
+    data(){
+        return{
+            iframeUrl: 'https://www.youtube.com/embed',
+            videoId: null,
+            videoType: null,
+            config: {
+                url: configArr.Global.url,
+                key: configArr.Global.parameters.key
+            }
         }
+    },
+    computed:{
+        //Check for whether video / playlist found or not
+        videoFound(){
+            if(this.videoId && this.videoType) return true;
+            return false;
+        }
+    },
+    methods:{
+        //Check for video/playlist ID & type
+        checkForId(id, type){
+            var vidType = null;
+
+            //If id and type are given set thm
+            if(type == 'video' || type == 'playlist' && id){
+                this.videoId = id;
+                this.videoType = type;
+                return false;
+            }
+
+            //First check if id is in route
+            if(this.$route.params.id){
+                vidType = this.getResponse(this.$route.params.id);
+                if(vidType) this.checkForId(id, vidType);
+            }
+        },
+        getResponse(id, type){
+            var responseTrue = false;
+            var returnVal = null;
+
+            if(!id) return returnVal;
+
+            if(!type || type == 'video'){
+                //First check if it is video or not
+                this.$http.get(this.config.url+'/videos', {params: {'id': id, 'key': this.config.key}}).then(resp => resp.json()).then(response => {
+                    if(response.items.length && response.kind == 'youtube#videoListResponse'){
+                       this.checkForId(id, 'video');
+                        return false;
+                    }
+
+                    this.getResponse(id, 'playlist');
+                }, err => {
+                    this.getResponse(id, 'playlist');
+                });
+            }
+            
+            if(type == 'playlist'){
+                //If still no result, search for playlist
+                this.$http.get(this.config.url+'/playlistItems', {params: {'playlistId': id, 'key': this.config.key}}).then(resp => resp.json()).then(response => {
+                     if(response.items.length && response.kind == 'youtube#playlistItemListResponse'){
+                       this.checkForId(id, 'playlist');
+                        return false;
+                    }
+                   this.getResponse();
+                }, err => {
+                    this.getResponse();
+                });
+            }
+        }
+    },
+    mounted(){
+        this.checkForId();
     }
 }
 </script>
