e28dce99acbd6efadaab68e4af05739ad70cec39
kgoel085 <kgoel085@gmail.com>
2019-01-03 01:14:30 +0530

    Added video id details and generate list

--- src/components/sections/extras/videoPlayer.vue
+++ src/components/sections/extras/videoPlayer.vue
@@ -1,16 +1,37 @@
 <template>
     <div class="row">
         <template v-if="!playlistGenerated">
-            <loader></loader>
+            <div class="col-md-12 text-center">
+                <loader></loader>
+            </div>
         </template>
         <template v-else>
-            <div class="col-md-9">
+            <div class="col-md-8">
                 <div id="videoPlyr"></div>
                 <p v-if="videoId !== null">Received Video id is {{ videoId }}</p>
                 <p v-if="playlistId !== null">Received Playlist id is {{ playlistId }}</p>
             </div>
-            <div class="col-md-3">
-                Playlist area
+            <div class="col-md-4">
+                <ul class="list-group">
+                    <li v-for="(vid, index) in videoDetailsArr" :key="index" class="list-group-item" @click="playVid(vid.vidId)">
+                        <template v-if="vid.title">
+                            <div class="media">
+                                <div class="media-left">
+                                    <a href="#">
+                                        <img class="media-object" :src="vid.thumbnails.medium.url" width="140px" height="auto">
+                                    </a>
+                                </div>
+                                <div class="media-body">
+                                    <span class="media-heading"><strong>{{ vid.title }}</strong></span><br>
+                                    <small>{{ vid.channelTitle }}</small>
+                                </div>
+                            </div>
+                        </template>
+                        <template v-else>
+                            <loader class="text-center"></loader>
+                        </template>
+                    </li>
+                </ul>
             </div>
         </template>
         
@@ -34,7 +55,8 @@ export default {
 
             playerFrame: null,
             playListArr: [],
-            currentPlayId: null
+            currentPlayId: null,
+            videoDetailsArr:[],
             
         }
     },
@@ -49,26 +71,27 @@ export default {
                 tmpArr = tmpArr.concat(this.playListArr);
             }
 
-            this.getVidDetails();
-
             return tmpArr;
         },
         playlistGenerated(){
-            if(this.videoIdArr.length > 0){
-                if(this.currentPlayId == null) this.createPlayer(this.videoIdArr[0]);
+            if(this.videoDetailsArr.length > 0){
+                this.createPlayer();
                 return true;
             }
 
             return false;
         }
     },
+    watch:{
+        videoIdArr(val){
+            this.getVidDetails(val);
+        }
+    },
     methods:{
         //Retrive all video id in a playlist
         getPlaylistVideos(val = this.playlistId, tmpArr = []){
             var vm = this;
-            if(tmpArr && tmpArr.length > 0) return tmpArr;
-
-            var paramsArr = {'playlistId': val, 'part': 'snippet,contentDetails','maxResults': 50, 'key': this.config.key, 'fields': 'items(snippet(resourceId/videoId))'};
+             var paramsArr = {'playlistId': val, 'part': 'snippet,contentDetails','maxResults': 50, 'key': this.config.key, 'fields': 'items(snippet(resourceId/videoId))'};
             this.$http.get(this.config.url+'/playlistItems', {'params': paramsArr}).then(response => response.json()).then(resp => {
                 if(resp.items && resp.items.length > 0){
                     var tmpIdArr = [];
@@ -77,15 +100,28 @@ export default {
                             if(element.snippet.resourceId.videoId) vm.playListArr.push(element.snippet.resourceId.videoId);
                         }
                    });
-
-                   if(tmpIdArr.length > 0) this.getPlaylistVideos(val, tmpIdArr);
                 }
             });
         },
 
         //Get all video details
-        getVidDetails(){
-            
+        getVidDetails(val = this.videoIdArr){
+            var vm = this;
+            if(this.videoIdArr.length > 0){
+                var videoIds = this.videoIdArr.join(',');
+                var paramsArr = {'id': videoIds, 'part': 'snippet,contentDetails','key': this.config.key};
+                this.$http.get(this.config.url+'/videos', {'params': paramsArr}).then(response => response.json()).then(resp => {
+                    if(resp.items && resp.items.length > 0){
+                            var tmpIdArr = [];
+                            resp.items.forEach(element => {
+                            if(element.snippet){
+                                element.snippet.id = element.id;
+                                vm.videoDetailsArr.push(element.snippet);
+                            }
+                        });
+                    }
+                });
+            }
         },
 
         //Get Playlist video id's
