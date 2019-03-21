2ffad8a2cedfadfdfcba639aacd0262fe8173e60
kgoel085 <kgoel085@gmail.com>
2019-03-22 03:13:22 +0530

    Added new Channel and carousel components Added Vue Owl Carousle component

---
+++ src/components/sections/Carousel.vue
@@ -0,0 +1,167 @@
+<template>
+    <div class='carousel-view' v-if="slides.length > 0">
+        <vue-carousel class="carousel" :dots="false" :rewind="false" :items="4" :nav="false" :key="screenSize">
+            <template slot="next"><button :class="'carousel-next btn btn-default btn-circle'"> <i class="glyphicon glyphicon-chevron-left"></i> </button></template>
+
+            <div class="thumbnail" v-for="(slide, index) in slides" :key="index" @click="showVideo(slide.videoId)">
+                <img :src="getImage(slide.thumbnails)" :alt="slide.title" :style="{width: '100%', height: currentHeight+'px'}">
+
+                <p class="caption" style="font-weight:550">{{ slide.title.substring(0,70) }}</p>
+
+                <kbd>
+                    <small v-if="slide.statistics.viewCount">{{ slide.statistics.viewCount | subscriberCount }} Views</small>
+                </kbd>
+                <kbd>
+                    <small v-if="slide.statistics.likeCount">{{ slide.statistics.likeCount | subscriberCount }} Likes</small>
+                    <small v-if="slide.statistics.dislikeCount">{{ slide.statistics.dislikeCount | subscriberCount }} Dislikes</small>
+                </kbd>
+            </div>
+    
+            <template slot="prev"><button :class="'carousel-prev btn btn-default btn-circle'"> <i class="glyphicon glyphicon-chevron-right"></i> </button></template>
+        </vue-carousel>
+    </div>
+</template>
+
+<script>
+import Carousel from 'vue-owl-carousel';
+
+export default {
+    data(){
+        return{
+            slides: [],
+            currentHeight: 0,
+            videoArr: []
+        }
+    },
+    computed:{
+        screenSize(){
+            return screen.availWidth;
+        }
+    },
+    watch:{
+        videoArr(val){
+            if(val && typeof(val) == 'object') this.getVideoDetails();
+        }
+    },
+    methods: {
+        //Get all the items in the playlist
+        getPlaylistItems(){
+            var vm = this;
+            var paramArr = {'part': 'contentDetails', 'maxResults': 8, 'fields': 'items(contentDetails(videoId))', 'key': this.config.key, 'playlistId': this.playlistId};
+
+            this.$http.get(this.config.url+'/playlistItems', {'params': paramArr}).then(response => response.json()).then(resp => {
+                if(resp.items && resp.items.length > 0){
+                    var respItems = resp.items;
+                    
+                    var tmpArr = [];
+                    respItems.forEach(element => {
+                        if(element.contentDetails.videoId){
+                            if($.inArray(element.contentDetails.videoId, tmpArr) < 0){
+                                tmpArr.push(element.contentDetails.videoId);
+                            }
+                        }
+                    });
+
+                    if(tmpArr.length > 0) vm.videoArr = tmpArr;
+                }
+            });
+        },
+
+        //Get all the detailsfor the videos in the playlist
+        getVideoDetails(){
+            var vm = this;
+            var videoIds = vm.videoArr.join(',');
+
+            var paramArr = {'part': 'snippet, statistics', 'fields':'items(id,snippet,statistics)','key': this.config.key, 'id': videoIds}; 
+            this.$http.get(this.config.url+'/videos', {'params': paramArr}).then(response => response.json()).then(resp => {
+                if(resp.items && resp.items.length > 0){
+                    var respItems = resp.items;
+
+                    respItems.forEach(element => {
+                        if(element.snippet){
+                            if(element.id) element.snippet['videoId'] = element.id;
+                            if(element.statistics) element.snippet.statistics = element.statistics
+                            vm.slides.push(element.snippet);
+                        }
+                    });
+                }
+            });
+        },
+
+        //Redirect to the selected video
+        showVideo(id){
+            //this.$store.dispatch('SHOW_VIDEO', id);
+            this.$router.push({ name: 'Player', query: { 'v': id } })
+        },
+
+        //Get video image based on the screen resolution
+        getImage(imgObj){
+            var returnImg = imgObj['default']['url'];
+
+            var defaultImageSize = 'default';
+            if(this.screenSize >= 320 ) defaultImageSize = 'medium';
+            if(this.screenSize >= 480 ) defaultImageSize = 'high';
+            if(this.screenSize >= 640 ) defaultImageSize = 'standard';
+            if(this.screenSize >= 1280 ) defaultImageSize = 'maxres';
+
+            if(imgObj[defaultImageSize]){
+                var currentObj = imgObj[defaultImageSize]['url'];
+                if(currentObj){
+                    returnImg = currentObj;
+                    this.currentHeight = imgObj['medium']['height'];
+                }
+            }
+
+            return returnImg;
+        }
+    },
+    mounted(){
+        if(this.playlistId) this.getPlaylistItems();
+    },
+    props:{
+        playlistId:{
+            type:String,
+            default: null
+        }
+    },
+    components:{
+        'vue-carousel': Carousel
+    }
+}
+</script>
+
+<style scoped>
+    .thumbnail{
+        margin-bottom: 0px;
+        background-color: #fff;
+        border: 1px solid transparent;
+        position:relative;
+    }
+
+    .thumbnail:hover{cursor:pointer;}
+
+    .carousel{
+        position:relative;
+    }
+
+    .btn-circle {
+        width: 40px;
+        height: 40px;
+        text-align: center;
+        position: absolute;
+        padding: 6px 0;
+        font-size: 12px;
+        line-height: 1.42;
+        border-radius: 100%;
+        top: 35%;
+        z-index: 9;
+    }
+
+    .carousel span:first-child > button{
+        right: -2%;
+    }
+
+    .carousel span:last-child > button{
+        left: -2%;
+    }
+</style>
---
+++ src/components/sections/Channel.vue
@@ -0,0 +1,138 @@
+<template>
+    <div class="col-md-12">
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <div class="media">
+                    <template v-if="Object.keys(channelObj).length > 0">
+                        <div class="media-left">
+                            <a href="#">
+                                <img class="media-object" :src="channelObj.thumbnails.high.url">
+                            </a>
+                        </div>
+                        <div class="media-body">
+                            <h4 class="title"><strong>{{ channelObj.title.replace(/\b\w/g, function(l){ return l.toUpperCase() }) }}</strong></h4>
+                        </div>
+                        <div class="media-right">
+                            <template v-if="channelObj.statistics">
+                                <button class="btn btn-danger" disabled>Subscribe {{ channelObj.statistics | subscriberCount }}</button>
+                            </template>
+                        </div>
+                    </template>
+                </div>
+            </div>
+            <div class="panel-body">
+                <template v-if="!vidView && playlistId !== null">
+                    <PlaylistCarousel :playlistId="playlistId"></PlaylistCarousel>
+                </template>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import PlaylistCarousel from '../sections/Carousel.vue';
+
+export default {
+    data(){
+        return{
+            channelObj: {},
+            playlistId: null,
+            currentId: this.catId
+        }
+    },
+    computed:{
+        vidView(){
+            if(this.playlistId) return false;
+            return true;
+        }
+    },
+    methods:{
+        //Get all the channel details
+        channelDetails(){
+            var vm = this;
+            var paramArr = {'part': 'snippet,contentDetails,statistics', 'key': this.config.key, 'id': this.currentId, 'fields':'items(id,snippet(title, publishedAt, thumbnails),statistics(hiddenSubscriberCount, subscriberCount), contentDetails)'};
+
+            this.$http.get(this.config.url+'/channels', {'params': paramArr}).then(response => response.json()).then(resp => {
+                if(resp.items && resp.items.length > 0){
+                    var respItems = resp.items;
+
+                    respItems.forEach(element => {
+                        if(element.snippet) vm.channelObj = element.snippet;
+                        if(element.statistics.subscriberCount) vm.channelObj.statistics = element.statistics.subscriberCount;
+
+                        //If playlist is there / else get any playlist for the current channel
+                        if(element.contentDetails){
+                            var playList = element.contentDetails.relatedPlaylists;
+                            if(playList.uploads) vm.playlistId = playList.uploads;
+                        }
+
+                        //If no playlist found get forcefully
+                        if(vm.playlistId === null) vm.getPlaylist();
+                        
+                    });
+                }
+            });
+        },
+
+        //Get playlist details for the current channel
+        getPlaylist(){
+            var vm = this;
+            var paramArr = {'part': 'snippet', 'key': this.config.key, 'channelId': this.currentId, 'maxResults': 1, 'fields': 'items(id)'};
+
+            this.$http.get(this.config.url+'/playlists', {'params': paramArr}).then(response => response.json()).then(resp => {
+                if(resp.items && resp.items.length > 0){
+                    var respItems = resp.items;
+
+                    respItems.forEach(element => {
+                        if(element.id) vm.playlistId = element.id;
+                    });
+                }
+            });
+        }
+    },
+    mounted(){
+        if(this.currentId) this.channelDetails();
+    },
+    props:{
+        catId:{
+            type: String,
+            default: null
+        }
+    },
+    components:{
+        PlaylistCarousel
+    }
+}
+</script>
+
+<style scoped>
+    .panel{
+        border:1px solid transparent;
+        border-bottom:1px solid #ddd;
+    }
+
+    .panel > .panel-heading{
+        border-bottom: 1px solid transparent;
+    }
+
+    .media-object{
+        display: block;
+        border-radius: 100%;
+        width: 32px;
+        height: auto;
+    }
+
+    .media-body, .media-left, .media-right{
+        vertical-align: middle!important;
+    }
+
+    .media-body .title{
+        font-weight: 500
+    }
+
+    .glyphicon{
+        color:#ccc;
+        cursor:pointer;
+        font-size:2em;
+    }
+</style>
