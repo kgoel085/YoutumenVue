010076000e2b9ed0315f551df1286347a772d77f
kgoel085 <kgoel085@gmail.com>
2019-03-24 02:07:23 +0530

    Minor function modifications

--- src/classes/filters.js
+++ src/classes/filters.js
@@ -10,15 +10,15 @@ Vue.filter('subscriberCount', function(value){
     // Nine Zeroes for Billions
     return Math.abs(Number(value)) >= 1.0e+9
     
-    ? (Math.abs(Number(value)) / 1.0e+9).toFixed(0) + "B"
+    ? (Math.abs(Number(value)) / 1.0e+9).toFixed(0) + " B"
     // Six Zeroes for Millions 
     : Math.abs(Number(value)) >= 1.0e+6
 
-    ? (Math.abs(Number(value)) / 1.0e+6).toFixed(0) + "M"
+    ? (Math.abs(Number(value)) / 1.0e+6).toFixed(0) + " M"
     // Three Zeroes for Thousands
     : Math.abs(Number(value)) >= 1.0e+3
 
-    ? (Math.abs(Number(value)) / 1.0e+3).toFixed(0) + "K"
+    ? (Math.abs(Number(value)) / 1.0e+3).toFixed(0) + " K"
 
     : Math.abs(Number(value));
 });
